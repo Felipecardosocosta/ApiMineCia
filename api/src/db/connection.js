62caa6350f83a34bee43c750d420a26887d29295
Felipecardosocosta <felipe.cardoso.costa96@gmail.com>
2025-11-24 23:14:27 -0300

    configurando

--- api/src/db/connection.js
+++ api/src/db/connection.js
@@ -3,10 +3,11 @@ const { createPool } = require("mysql2/promise")
 require("dotenv").config()
 
 const pool = createPool({
-    host: "localhost",
-    user: 'root',      
+    host: process.env.HOST,
+    user: 'root',
+    port: process.env.PORT,    
     password: process.env.SENHA_BD,
-    database: 'mini_cia_e_trilhas',
+    database: process.env.DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
